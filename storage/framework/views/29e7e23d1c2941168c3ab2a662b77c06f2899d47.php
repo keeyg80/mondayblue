76f7948d03c9adcb12844821ba6bb243ce03ccfd
unknown <keeyg80@gmail.com>
2020-09-26 10:25:17 +0800

    update about us

--- storage/framework/views/29e7e23d1c2941168c3ab2a662b77c06f2899d47.php
+++ storage/framework/views/29e7e23d1c2941168c3ab2a662b77c06f2899d47.php
@@ -1,78 +1,211 @@
 
 
-
 <?php $__env->startSection('content'); ?>
-    
+    <style>
+        .inner {
+            overflow: hidden;
+
+        }
+
+        .inner img {
+            transition: all 1.75s ease;
+        }
+
+        .inner:hover img {
+            transform: scale(1.2);
+        }
+
+        .image-resize {
+            height: 200px;
+            object-fit: cover;
+            object-position: center center;
+        }
+
+    </style>
     <!-- <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-        <h1 class="display-4 font-italic">Find & Hire Expert Freelancers</h1>
-        <p class="lead my-3">Work with the best freelance talent from around the world on our secure,
-            flexible and cost-effective platform.</p>
-        <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
-        </div>
-    </div> -->
+                                <div class="col-md-6 px-0">
+                                <h1 class="display-4 font-italic">Find & Hire Expert Freelancers</h1>
+                                <p class="lead my-3">Work with the best freelance talent from around the world on our secure,
+                                    flexible and cost-effective platform.</p>
+                                <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+                                </div>
+                            </div> -->
     <div id="carouselExampleIndicators" class="mt-5 carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
         <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                        <div class="img" style="height:300px;background-color:#7F8C8D"><img class="d-block img-fluid"  >
-                                <div class="container mt-5">
-                                        <h1>Example 1.</h1>
-                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                                </div>
-                        </div>
+            <div class="carousel-item active">
+                <div class="img" style="height:300px;background-color:#7F8C8D"><img class="d-block img-fluid">
+                    <div class="container mt-5">
+                        <h1>Example 1.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida
+                            at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
                 </div>
+            </div>
 
-                <div class="carousel-item">
-                        <div class="img" style="height:300px;background-color:#F5B041"><img class="d-block img-fluid"  >
-                                <div class="container mt-5">
-                                        <h1>Example 2.</h1>
-                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                                </div>
-                        </div>
+            <div class="carousel-item">
+                <div class="img" style="height:300px;background-color:#F5B041"><img class="d-block img-fluid">
+                    <div class="container mt-5">
+                        <h1>Example 2.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida
+                            at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
                 </div>
+            </div>
 
-                <div class="carousel-item">
-                        <div class="img" style="height:300px;background-color:#5DADE2"><img class="d-block img-fluid"  >
-                                <div class="container mt-5">
-                                        <h1>Example 3.</h1>
-                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                                </div>
-                        </div>
+            <div class="carousel-item">
+                <div class="img" style="height:300px;background-color:#5DADE2"><img class="d-block img-fluid">
+                    <div class="container mt-5">
+                        <h1>Example 3.</h1>
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida
+                            at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
                 </div>
+            </div>
         </div>
 
-        
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-                </a>
-        </div>
-        
-        <div class="content">
-                
-                <div class="container flex-top position-ref full-height">
-                        <div class="row">
-                                <div class="col-xl-4 d-flex justify-content-left text-left mt-5">
-                                        <div class="img"><img src="img/Company_logo.png" ></div>
-                                </div>
-                                <div class="col-xl-8 d-flex justify-content-left text-left mt-5">    
-                                        <div class="row ">
-                                                <h1>Company Overview</h1>
-                                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolore maiores adipisci cum quidem. Corrupti error impedit, quo pariatur hic in excepturi, enim voluptate commodi eligendi doloremque animi dolores libero?</p>               
-                                        </div>
-                                </div>   
-                        </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="content">
+
+        <div class="container flex-top position-ref full-height">
+            <div class="row">
+                <div class="col-xl-4 d-flex justify-content-left text-left mt-5">
+                    <div class="img"><img src="img/Company_logo.png"></div>
                 </div>
+                <div class="col-xl-8 d-flex justify-content-left text-left mt-5">
+                    <div class="row ">
+                        <h1>Company Overview</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolore maiores adipisci cum
+                            quidem. Corrupti error impedit, quo pariatur hic in excepturi, enim voluptate commodi eligendi
+                            doloremque animi dolores libero?</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <h2 style="font-size: 28px; line-height: 40px;">Our Story</h2>
+            <p style="font-size: 18px; line-height: 30px;" class="sub-heading"></p>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis ligula eu lectus vulputate
+                    porttitor sed feugiat nunc. Mauris ac consectetur ante, dapibus gravida tellus. Nullam aliquet eleifend
+                    dapibus. Cras sagittis, ex euismod lacinia tempor, lectus orci elementum augue, eget auctor metus ante
+                    sit amet velit.</p>
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis ligula eu lectus vulputate
+                    porttitor sed feugiat nunc. Mauris ac consectetur ante, dapibus gravida tellus. Nullam aliquet eleifend
+                    dapibus. Cras sagittis, ex euismod lacinia tempor, lectus orci elementum augue, eget auctor metus ante
+                    sit amet velit.</p>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <h2 style="font-size: 28px; line-height: 40px;">Our Values</h2>
+            <p style="font-size: 18px; line-height: 30px;" class="sub-heading"></p>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-12">
+                <div class="card h-100">
+                    <img src="https://placeimg.com/600/500/tech/sepia" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Quality</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. In quas consequuntur
+                            ipsa et veritatis, odit soluta, tempore magni cumque, laboriosam similique expedita error sequi
+                            nobis culpa tenetur ad quibusdam repellat.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="card h-100">
+                    <img src="https://placeimg.com/600/500/people/sepia" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Respect</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. In quas consequuntur
+                            ipsa et veritatis, odit soluta, tempore magni cumque, laboriosam similique expedita error sequi
+                            nobis culpa tenetur ad quibusdam repellat.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <div class="card h-100">
+                    <img src="https://placeimg.com/600/500/arc/grayscale" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Responsibility</h5>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. In quas consequuntur
+                            ipsa et veritatis, odit soluta, tempore magni cumque, laboriosam similique expedita error sequi
+                            nobis culpa tenetur ad quibusdam repellat.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <h2 style="font-size: 28px; line-height: 40px;">Our Team</h2>
+            <p style="font-size: 18px; line-height: 30px;" class="sub-heading"></p>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6">
+                <div class="card border-0 h-100">
+                    <img src="https://placeimg.com/600/500/people" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">John Doe</h5>
+                        <p class="card-text">Managing Director</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card border-0 h-100">
+                    <img src="https://placeimg.com/600/500/arc/sepia" class="card-img-top  image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Hugo Bash</h5>
+                        <p class="card-text">IT Head</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card border-0 h-100">
+                    <img src="https://placeimg.com/600/500/arc" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Jane Doe</h5>
+                        <p class="card-text">Chief Designer</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="card border-0 h-100">
+                    <img src="https://placeimg.com/600/500/nature/grayscale" class="card-img-top image-resize" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Barney Dino</h5>
+                        <p class="card-text">Technician</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\mondayblue\resources\views/pages/about.blade.php ENDPATH**/ ?>
